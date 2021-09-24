2325e2eb361e94bbfa341b1f4dc9fa859f758d30
Ty <1185425962@qq.com>
2021-09-24 11:16:49 +0800

    .

--- src/ServiceInstaller.php
+++ src/ServiceInstaller.php
@@ -11,14 +11,14 @@ class ServiceInstaller extends LibraryInstaller
      */
     public function getInstallPath(PackageInterface $package)
     {
-        $commonPlugin = ['xinmo/rpc', 'xinmo/jobcenter'];
+        $commonPlugin = ['ty-php/rpc', 'ty-php/jobcenter'];
         if (in_array($package->getPrettyName(), $commonPlugin)) {
             return 'common/' . substr($package->getPrettyName(), 6);
         }
 
         $prefix = substr($package->getPrettyName(), 0, 14);
-        if ('xinmo/service-' !== $prefix) {
-            throw new \InvalidArgumentException('The package name:' . $prefix . ',unable to installed, should always start their package name with "xinmo/service-"');
+        if ('ty-php/service-' !== $prefix) {
+            throw new \InvalidArgumentException('The package name:' . $prefix . ',unable to installed, should always start their package name with "ty-php/service-"');
         }
 
         return 'common/service/' . substr($package->getPrettyName(), 14);
